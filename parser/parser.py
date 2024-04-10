1044820515915f338c465342e1a68eed893f46c4
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2024-04-09 22:44:29 -0400

    Parse meta and raw; Tests

--- parser/parser.py
+++ parser/parser.py
@@ -1,6 +1,8 @@
-from typing import Dict
-import pandas as pd
+from io import StringIO
 from pathlib import Path
+from typing import Dict
+
+import pandas as pd
 
 
 class Chromatogram:
@@ -11,20 +13,36 @@ class Chromatogram:
             raise FileNotFoundError(f"No file found at {filepath}")
 
         self.metadata: Dict[str, Dict[str, str]] = {
-            'Injection Information': {},
-            'Chromatogram Data Information': {},
-            'Signal Parameter Information': {}
+            "Injection Information": {},
+            "Chromatogram Data Information": {},
+            "Signal Parameter Information": {},
         }
-        self.raw_data: pd.DataFrame = pd.DataFrame(columns=['Time (min)', 'Step (s)', 'Value (EU)'])
+        self.raw_data: pd.DataFrame = pd.DataFrame()
         self._parse_file(filepath)
 
-    def _parse_file(self, filepath: Path) -> None:
-        # Non None but whatever
-        pass
+    def _parse_file(self, filepath: Path):
+        with filepath.open("r") as file:
+            content = file.read()
+
+        metadata, data = content.split("Chromatogram Data:\n")
+        current_section = "Other"
+
+        for line in metadata.split("\n"):
+            line = line.strip()
+            if line in [
+                "Injection Information:",
+                "Chromatogram Data Information:",
+                "Signal Parameter Information:",
+            ]:
+                current_section = line[:-1]
+            elif line:
+                key, value = line.split("\t")
+                self.metadata[current_section][key] = value
+
+        self.raw_data = pd.read_csv(StringIO(data), sep="\t")
 
     def detect_peaks(self) -> None:
         pass
 
     def calculate_elution_volume(self) -> None:
         pass
-
---
+++ tests/test_parser.py
@@ -0,0 +1,29 @@
+from parser.parser import Chromatogram
+from pathlib import Path
+
+import pandas as pd
+
+
+def test_parse_file():
+    filepath = Path(__file__).parent.parent / "data" / "IgG Vtag 1_ACQUITY FLR ChA.txt"
+    chrom = Chromatogram(filepath)
+
+    # Metadata parsing
+    assert "Injection Information" in chrom.metadata
+    assert "Chromatogram Data Information" in chrom.metadata
+    assert "Signal Parameter Information" in chrom.metadata
+
+    assert chrom.metadata["Injection Information"]["Data Vault"] == "ChromeleonLocal"
+    assert (
+        chrom.metadata["Chromatogram Data Information"]["Time Min. (min)"] == "0.000000"
+    )
+    assert chrom.metadata["Signal Parameter Information"]["Signal Info"] == "techCHROM"
+
+    # Raw data parsing
+    assert not chrom.raw_data.empty
+    assert isinstance(chrom.raw_data, pd.DataFrame)
+
+    # Check first row
+    assert chrom.raw_data.iloc[0]["Time (min)"] == 0.0
+    # assert chrom.raw_data.iloc[0]['Step (s)'] == 0.5
+    assert chrom.raw_data.iloc[0]["Value (EU)"] == 0.0
