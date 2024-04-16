8e1eeaeea2ece0219df810b68786021148053efa
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2024-04-15 22:26:05 -0400

    Refactor; Add checks

--- parser/chromatogram.py
+++ parser/chromatogram.py
@@ -9,10 +9,6 @@ from scipy.integrate import simps
 from scipy.signal import find_peaks, savgol_filter
 
 
-def calculate_elution_volume(peak: Peak, flow_rate: float):
-    return peak.retention_time * flow_rate
-
-
 class Chromatogram:
     def __init__(self, filepath: Path):
         if not isinstance(filepath, Path):
@@ -55,7 +51,9 @@ class Chromatogram:
         try:
             metadata, data = content.split("Chromatogram Data:\n")
         except ValueError:
-            raise ValueError("File format incorrect or 'Chromatogram Data:' section missing")
+            raise ValueError(
+                "File format incorrect or 'Chromatogram Data:' section missing"
+            )
 
         current_section = "Other"  # TODO What if it is in the middle
 
@@ -76,12 +74,16 @@ class Chromatogram:
         except KeyError as e:
             raise ValueError(f"Expected column missing from the data: {e}")
 
-    def detect_peaks(self, degree: int = 3, min_height: float = None):
+    def detect_peaks(self, poly_degree: int = 3, min_height: float = None, window_length: int = 25):
         values = self.raw_data["Value (EU)"].to_numpy()
         time = self.raw_data["Time (min)"].to_numpy()
+        if len(values) < window_length:
+            raise ValueError("Window size for Savitzky-Golay filter must be less than the length of the dataset")
+        if window_length % 2 == 0:
+            raise ValueError("Window size for Savitzky-Golay filter must be must be an odd number")
 
-        # Polynomial Fit for Detrending
-        coeffs = np.polyfit(time, values, deg=degree)
+        # Polynomial fit for detrending
+        coeffs = np.polyfit(time, values, deg=poly_degree)
         baseline_func = np.poly1d(coeffs)
         baseline = baseline_func(time)
 
@@ -89,7 +91,7 @@ class Chromatogram:
         detrended_data = values - baseline
 
         # Apply Savitzky-Golay filter to smooth the detrended data
-        values = savgol_filter(detrended_data, window_length=51, polyorder=3)
+        values = savgol_filter(detrended_data, window_length, polyorder=3)
 
         if min_height is None:
             # Setting minimum height to be more than one standard deviation above the mean to focus on significant peaks
@@ -101,29 +103,39 @@ class Chromatogram:
 
         for i, peak in enumerate(peaks):
             # Get peak height
-            peak_height = properties['peak_heights'][i]
+            peak_height = properties["peak_heights"][i]
             retention_time = time[peak]
 
             # Find the left and right base indices
-            left_base_idx = properties['left_bases'][i]
-            right_base_idx = properties['right_bases'][i]
+            left_base_idx = properties["left_bases"][i]
+            right_base_idx = properties["right_bases"][i]
+
+            # Slice the data for the peak
+            peak_data = self.raw_data.iloc[left_base_idx: right_base_idx + 1]
 
             self.peaks.append(
                 Peak(
-                    time[left_base_idx],
-                    time[right_base_idx],
+                    left_base_idx,
+                    right_base_idx,
                     peak_height,
-                    retention_time
+                    retention_time,
+                    peak_data,
                 )
             )
 
-    # def calculate_peak_area(self, peak: Peak):
-    #     values = self.raw_data["Value (EU)"].to_numpy()
-    #     time = self.raw_data["Time (min)"].to_numpy()
-    #     return simps(
-    #             y=values[left_thresh_idx:right_thresh_idx + 1] - self.baseline[peak.retention_time],
-    #             x=time[left_thresh_idx:right_thresh_idx + 1],
-    #         )
+    def calculate_peak_area(self, peak: Peak):
+        # Calculating the area under the curve for the peak data using Simpson's rule
+        if peak.data.empty:
+            raise ValueError("Peak data is empty, cannot calculate area.")
+        area = simps(peak.data_slice["Value (EU)"], peak.data_slice["Time (min)"])
+        return area
+
+    def calculate_elution_volume(self, peak: Peak, flow_rate: float):
+        # Calculating elution volume based on the retention time and flow rate
+        if flow_rate <= 0:
+            raise ValueError("Flow rate must be a positive number.")
+        elution_volume = peak.retention_time * flow_rate
+        return elution_volume
 
 
 if __name__ == "__main__":
--- parser/peak.py
+++ parser/peak.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Peak:
     def __init__(
         self,
@@ -5,14 +7,17 @@ class Peak:
         right_thresh: float,
         height: float,
         retention_time: float,
+        data: pd.DataFrame,
     ):
         self.left_thresh = left_thresh
         self.right_thresh = right_thresh
         self.height = height
         self.retention_time = retention_time
+        self.data = data
 
     def __repr__(self):
         return (
             f"Peak(left_thresh={self.left_thresh}, right_thresh={self.right_thresh}, "
-            f"height={self.height}, retention_time={self.retention_time}"
+            f"height={self.height}, retention_time={self.retention_time}, "
+            f"data=[{self.data.to_string(index=False)}])"
         )
